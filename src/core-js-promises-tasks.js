c84eca69e8aed5831b1ac73f836f85c424fbaba9
EVISION\Otabek.Mirzakhmedov <otabek.mirzakhmedov@wolterskluwer.com>
2024-04-12 14:06:22 +0500

    get first promise result

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -89,8 +89,15 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return promises
+    .reduce((acc, promise) => {
+      return acc.catch(() => promise);
+    }, Promise.reject())
+    .then((value) => value)
+    .catch((reason) => {
+      throw reason;
+    });
 }
 
 /**
